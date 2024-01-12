01e36314510154ab5ae861167146a09e540f918d
Jose Caudillo <caudillojose5@gmail.com>
2024-01-11 19:43:54 -0800

    feat: added linkedlist implentation in typescript

---
+++ DataStructures/linkedlist.ts
@@ -0,0 +1,29 @@
+ class ListNode {
+     val: number
+     next: ListNode | null
+     constructor(val?: number, next?: ListNode | null) {
+         this.val = (val===undefined ? 0 : val)
+         this.next = (next===undefined ? null : next)
+     }
+ }
+
+ head: ListNode = new ListNode(2,null);
+ 
+
+class LinkedList {
+    head: ListNode | null;
+
+    constructor(){
+        this.head = null;
+    }
+
+    //Add an element to the end of the list 
+    push
+}
+
+
+
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    return null;
+};
